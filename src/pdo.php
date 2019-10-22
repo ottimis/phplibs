575027467c032d4610b9cf398fdfb881a7196c62
mmoriani <mmoriani@MacBook-Pro-di-MM.local>
2019-10-22 13:57:07 +0200

    PDO lib

---
+++ src/pdo.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace ottimis\phplibs;
+
+	class pdo	{
+
+		protected $host = '';
+		protected $user = '';
+		protected $password = '';
+		protected $database = '';
+		protected $port = '';
+		protected $persistent = false;
+		protected $preparedSql = NULL;
+		protected $conn = NULL;
+		protected $result= false;
+		protected $error_reporting = true;
+
+
+        function __construct(array $db)
+        {
+            $this->host = isset($db) ? getenv('DB_HOST_' . $db) : getenv('DB_HOST');
+            $this->user = isset($db) ? getenv('DB_USER_' . $db) : getenv('DB_USER');
+            $this->password = isset($db) ? getenv('DB_PASSWORD_' . $db) : getenv('DB_PASSWORD');
+            $this->database = isset($db) ? getenv('DB_NAME_' . $db) : getenv('DB_NAME');
+            $this->port = isset($db) ? getenv('DB_PORT_' . $db) : getenv('DB_PORT');
+
+            $this->conn = new PDO("dblib:host=$this->host:$this->port;dbname=$this->database", "$this->user", "$this->password");
+            return $this->conn;
+        }
+
+		// se error_reporting attivato riporto errore
+		function error() {
+			return $this->conn->errorInfo();
+		}
+
+		// gruppo funzioni interrogazione
+		function query($sql) {
+			foreach ($this->conn->query($sql) as $row) {
+				$ar[] = $row;
+			}
+			if (sizeof($ar) == 1)	{
+				$ar = $row;
+			}
+			$this->result = $ar;
+			return($ar);
+		}
+
+		function prepare($sql) {
+			$this->$preparedSql = $this->conn->prepare($sql);
+		}
+
+		function execute($ar) {
+			return $this->preparedSql->execute($ar);
+		}
+
+		function numrows() {
+			return $this->conn->rowCount();
+		}
+
+
+		function insert_id()	{
+			return $this->conn->lastInsertId();
+		}
+
+	}
+?>
