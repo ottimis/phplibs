c4ea9f5e3d368fc40da1cff9e783f5ea861cfada
mmoriani <mm@ottimis.com>
2021-10-22 18:32:16 +0200

    ***Notify***

---
+++ src/Notify.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ottimis\phplibs;
+
+class Notify
+{
+    const ALERT = 1;
+
+    public function __construct()
+    {
+    }
+
+    public static function notify($title, $data = array())
+    {
+      $headers = array('Content-Type: application/json');
+      $ar = array(
+        "idtype" => Notify::ALERT,
+        "title" => $title,
+        "data" => $data,
+        "service_from" => $_SERVER['HTTP_HOST']
+      );
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, "https://notify.ottimis.com/alarm");
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($ar));
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+      $server_output = curl_exec($ch);
+      $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      curl_close($ch);
+
+      return array(
+          "body" => $server_output,
+          "statusCode" => $statusCode
+      );
+    }
+}
