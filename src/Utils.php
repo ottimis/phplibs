0c996ae4b287632172498f6ee3823453fe1df32c
mmoriani <mmoriani95@gmail.com>
2023-08-23 13:31:14 +0200

    dbSql refactoring

--- src/Utils.php
+++ src/Utils.php
@@ -12,64 +12,6 @@ class Utils
    * @var Logger | @var LoggerPdo
    */
   public $Log;
-  protected $httpCodes = array(
-    100 => 'Continue',
-    101 => 'Switching Protocols',
-    102 => 'Processing',
-    103 => 'Checkpoint',
-    200 => 'OK',
-    201 => 'Created',
-    202 => 'Accepted',
-    203 => 'Non-Authoritative Information',
-    204 => 'No Content',
-    205 => 'Reset Content',
-    206 => 'Partial Content',
-    207 => 'Multi-Status',
-    300 => 'Multiple Choices',
-    301 => 'Moved Permanently',
-    302 => 'Found',
-    303 => 'See Other',
-    304 => 'Not Modified',
-    305 => 'Use Proxy',
-    306 => 'Switch Proxy',
-    307 => 'Temporary Redirect',
-    400 => 'Bad Request',
-    401 => 'Unauthorized',
-    402 => 'Payment Required',
-    403 => 'Forbidden',
-    404 => 'Not Found',
-    405 => 'Method Not Allowed',
-    406 => 'Not Acceptable',
-    407 => 'Proxy Authentication Required',
-    408 => 'Request Timeout',
-    409 => 'Conflict',
-    410 => 'Gone',
-    411 => 'Length Required',
-    412 => 'Precondition Failed',
-    413 => 'Request Entity Too Large',
-    414 => 'Request-URI Too Long',
-    415 => 'Unsupported Media Type',
-    416 => 'Requested Range Not Satisfiable',
-    417 => 'Expectation Failed',
-    418 => 'I\'m a teapot',
-    422 => 'Unprocessable Entity',
-    423 => 'Locked',
-    424 => 'Failed Dependency',
-    425 => 'Unordered Collection',
-    426 => 'Upgrade Required',
-    449 => 'Retry With',
-    450 => 'Blocked by Windows Parental Controls',
-    500 => 'Internal Server Error',
-    501 => 'Not Implemented',
-    502 => 'Bad Gateway',
-    503 => 'Service Unavailable',
-    504 => 'Gateway Timeout',
-    505 => 'HTTP Version Not Supported',
-    506 => 'Variant Also Negotiates',
-    507 => 'Insufficient Storage',
-    509 => 'Bandwidth Limit Exceeded',
-    510 => 'Not Extended'
-  );
 
   public function __construct($dbname = "")
   {
@@ -81,49 +23,34 @@ class Utils
   public function dbSql($bInsert, $table, $ar, $idfield = "", $idvalue = "", $noUpdate = false, $preventEmptyStringOnNull = true)
   {
     $db = $this->dataBase;
-    $values = '';
-    $z = '';
+
+    // Filter special keys like "now()" and null
+    $ar = array_map(function ($value) use ($db) {
+      $value = match ($value) {
+        'now()' => "now()",
+        null => "NULL",
+        default => "'" . $db->real_escape_string($value) . "'",
+      };
+      return $value;
+    }, $ar);
+
+    // Merge $key + "=" + $value
+    $mergedAr = array();
+    foreach ($ar as $k => $v) {
+      $mergedAr[] = "$k=$v";
+    }
+    $mergedValues = implode(", ", $mergedAr);
 
     try {
       if ($bInsert) {
         $columns = implode(", ", array_keys($ar));
-        foreach ($ar as $k) {
-          if ($preventEmptyStringOnNull && is_null($k)) {
-            continue;
-          }
-          $values .= $values != '' ? "," : "";
-          if ($k !== "now()") {
-            $values .= "'" . $db->real_escape_string($k) . "'";
-          } else {
-            $values .= "now()";
-          }
-        }
-        foreach ($ar as $k => $v) {
-          $z .= $z != '' ? "," : "";
-          if ($v !== "now()") {
-            $z .= $k . "='" . $db->real_escape_string($v) . "'";
-          } else {
-            $z .= $k . "=now()";
-          }
-        }
+        $values = implode(", ", $ar);
         $sql = "INSERT INTO $table ($columns) VALUES ($values)";
         if (!$noUpdate) {
-          $sql .= " ON DUPLICATE KEY UPDATE $z";
+          $sql .= " ON DUPLICATE KEY UPDATE $mergedValues";
         }
       } else {
-        $z = "";
-        foreach ($ar as $k => $v) {
-          if ($preventEmptyStringOnNull && is_null($k)) {
-            continue;
-          }
-          $z .= ($z != "") ? "," : "";
-          if ($v !== "now()") {
-            $z .= $k . "='" . $db->real_escape_string($v) . "'";
-          } else {
-            $z .= $k . "=now()";
-          }
-        }
-        $sql = sprintf("UPDATE %s SET %s WHERE %s='%s'", $table, $z, $idfield, $idvalue);
+        $sql = sprintf("UPDATE %s SET %s WHERE %s='%s'", $table, $mergedValues, $idfield, $idvalue);
       }
 
       $ret['sql'] = $sql;
